88c836b4beaf30913c0a39136f7efced3bd417c3
Montserrat Huato <montserrathuato12@gmail.com>
2025-11-26 23:04:50 -0600

    feat(ui/login): diseño completo de pantalla de login

---
+++ lib/src/colors/colors.dart
@@ -0,0 +1,7 @@
+
+import 'dart:ui';
+
+final amarillo = Color.fromRGBO(255, 204, 0, 1.0);
+final   gris = Color.fromRGBO(184, 187, 198, 1.0);
+final rosa = Color.fromRGBO(255, 45, 85, 1.0);
+final bgGray = Color.fromRGBO(247, 248, 250, 1.0);
