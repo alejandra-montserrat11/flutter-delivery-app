c4360b7aaa237c9ff793b0c866f21c30465ee88a
Montserrat Huato <montserrathuato12@gmail.com>
2025-11-27 00:16:18 -0600

    feat(ui/forgot-password): diseño de pantalla de recuperación de contraseña

--- lib/src/colors/colors.dart
+++ lib/src/colors/colors.dart
@@ -5,3 +5,4 @@ final amarillo = Color.fromRGBO(255, 204, 0, 1.0);
 final   gris = Color.fromRGBO(184, 187, 198, 1.0);
 final rosa = Color.fromRGBO(255, 45, 85, 1.0);
 final bgGray = Color.fromRGBO(247, 248, 250, 1.0);
+final grisClaro = Color.fromRGBO(213, 213, 212, 1.2);
